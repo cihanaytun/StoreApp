5e2df08bd1550ab6a86841af6f9b4827f87ae9a1
cihanaytun <aytuncihan@gmail.com>
2022-06-24 19:02:45 +0300

    Add project files.

---
+++ storeAPI/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using storeInfrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+        public ProductController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduct()
+        {
+            var product = await _storeContext.Products.ToListAsync();
+            if (product == null)
+            {
+                return StatusCode(500);
+            }
+            return StatusCode(200, product);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _storeContext.Products.FindAsync(id);
+
+            if (product.Id != id)
+            {
+                return StatusCode(500);
+            }
+            return StatusCode(200, product);
+        }
+
+    }
+}
---
+++ storeInfrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using storeCore.Entities;
+
+namespace storeInfrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext( DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
